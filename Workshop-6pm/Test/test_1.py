58029acba5c1df64a1530df17197d5a71997b434
Anna <an.harutyunova@gmail.com>
2024-06-20 00:11:21 +0400

    Add my comments

--- Workshop-6pm/Test/test_1.py
+++ Workshop-6pm/Test/test_1.py
@@ -14,10 +14,28 @@ def test_search_product(driver):
     results = product_page.get_results()
     logging.info(f"Geting search result {results}")
     logging.info("Checking search result suit to searched data")
-    assert ({test_data.brand_name} in result for result in results), "No Lacoste sunglasses found"
+    assert all(test_data.brand_name  in result for result in results), "No Lacoste sunglasses found"
     logging.info(f"Brand: {test_data.brand_name} is in searched data: {results}")
-    assert ({test_data.price_range} in result and float(
-        result.split("$")[1]) <= 200.00 for result in results), "Price filter not applied correctly"
+    assert all(float(result.split("$")[1].split(" ")[0].strip('.')) <= 200.00 for result in results)
     logging.info(f"Price: {test_data.price_range} is in searched data: {results}")
     logging.info("Result information suit to searched data")
     logging.info("Test passed")
+
+
+# TODO
+# 1. Locators in product_page.py is not effective, need to improve
+# Thats good that you keep price and brand in test_data.py, but you could use that data in locators as well.
+# e.g.: write brand and price locators In functions where you use them
+"""
+    def your_fucntion(self, brand_name, price):
+        brand_name_filter = (By.XPATH, f"//ul[@aria-labelledby='brandNameFacet']//span[text()='{brand_name}']")
+        brand_price_filter = (By.XPATH, f"//a[@class='uA-z']//span[text()='${price} and Under']")
+        ....
+        """
+# And then in the test case you can call the function with the data from test_data.py
+# I changed assert in test cases, the main issue was that you used assert without all() function 
+# which is not correct because without all() functionthe expression inside the assertion will become a generator expression,
+#  which is always True. So, I added all() function to the assert statements.
+# I removed checking of price range in the result. Just checked that the price is less than 200.00 (will be better not hardcode price in the test)
+
+
